a66fc65599874db4b9c241af5180fd40f3be2d77
kimyeonbin <dusqls76@naver.com>
2023-07-04 15:34:26 +0900

    js 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,5 @@ $(function(){
     //a태그를_버튼으로_작동
     $('.sc-news .btn-prev, .sc-themecast .group-category .btn, .shop-control .btn').click(function(e){
         e.preventDefault();
-    })
+    });
 });
