f12734372f5febe49bdc04b5b1a6fd8b47110663
kimyeonbin <dusqls76@naver.com>
2023-07-04 15:34:05 +0900

    a태그 새로고침을 막는 js 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,8 @@ $(function(){
         e.preventDefault();
         window.scrollTo({top: 0, behavior: "smooth"});
     });
+    //a태그를_버튼으로_작동
+    $('.sc-news .btn-prev, .sc-themecast .group-category .btn, .shop-control .btn').click(function(e){
+        e.preventDefault();
+    })
 });
